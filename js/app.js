489521e344840c30cd825dbea69e2f2dd683e2dd
Zerubabbel <achiappetta90@aol.com>
2015-03-27 11:17:16 -0400

    updates

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+    $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+      }   
+    })
+
+
+});
+
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
+
+
