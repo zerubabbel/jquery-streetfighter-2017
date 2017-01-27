5dbd0f33c134e6eee3d8550bb17c60d38557069d
zerubabbel <achiappetta90@gmail.com>
2017-01-26 20:59:14 -0800

    repairs

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.ryu').mouseenter(function(){
         $('.ryu-still').hide();
         $('.ryu-ready').show();
+
     })
     .mouseleave(function() {
         $('.ryu-still').show();
